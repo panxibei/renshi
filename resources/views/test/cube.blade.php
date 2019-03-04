f6541c8164e9b2374cd75fa23a650e0aa10d4b5b
panxibei <panxibei@yandex.com>
2019-03-04 16:30:02 +0800

    renshi

--- resources/views/test/cube.blade.php
+++ resources/views/test/cube.blade.php
@@ -2,6 +2,10 @@
 <html lang="en">
 <head>
 	<meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
 	<title>Test Cube</title>
     <!-- 引入样式 -->
     <link rel="stylesheet" href="{{ asset('statics/cube/cube.min.css') }}">
