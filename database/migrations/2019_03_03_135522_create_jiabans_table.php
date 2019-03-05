d48620de05b1fcb0b6f7e376ea677f00fc67cd63
panxibei <panxibei@yandex.com>
2019-03-05 11:58:54 +0800

    renshi

--- database/migrations/2019_03_03_135522_create_jiabans_table.php
+++ database/migrations/2019_03_03_135522_create_jiabans_table.php
@@ -13,14 +13,39 @@ class CreateJiabansTable extends Migration
      */
     public function up()
     {
-        Schema::create('jiabans', function (Blueprint $table) {
+        Schema::create('renshi_jiaban_mains', function (Blueprint $table) {
             $table->bigIncrements('id');
-            $table->text('main_id')->unique()->comment('主编号');
+            $table->string('main_id')->unique()->comment('主编号');
+            $table->string('agent')->comment('代理申请人');
+            $table->string('department')->comment('代理申请部门');
+            $table->timestamps();
+        });
+
+        Schema::create('renshi_jiaban_subs', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('sub_id')->unique()->comment('主编号');
             $table->string('agent')->comment('代理申请人');
             $table->string('department')->comment('代理申请部门');
             $table->json('info');
 
             $table->timestamps();
+        });
+
+        Schema::create($tableNames['role_has_permissions'], function (Blueprint $table) use ($tableNames) {
+            $table->unsignedInteger('permission_id');
+            $table->unsignedInteger('role_id');
+
+            $table->foreign('permission_id')
+                ->references('id')
+                ->on($tableNames['permissions'])
+                ->onDelete('cascade');
+
+            $table->foreign('role_id')
+                ->references('id')
+                ->on($tableNames['roles'])
+                ->onDelete('cascade');
+
+            $table->primary(['permission_id', 'role_id']);
         });
     }
 
--- database/seeds/JiabansTableSeeder.php
+++ database/seeds/JiabansTableSeeder.php
@@ -17,58 +17,46 @@ class JiabansTableSeeder extends Seeder
 		$nowtime = date("Y-m-d H:i:s",time());
 		
         Jiaban::truncate();
-        
+
+        // json数据写入，通过 '{"key1": "value1", "key2": "value2"}' 的形式。
+        // 数据库最好用jsonb类型。
         Jiaban::create([
             'main_id' => 'MJB000000001',
             'agent' => 'zhangsan',
             'department' => 'shengchanbu',
-            'info' => json_encode([
-                [
-                'main_id' => 'MJB000000001',
-                'sub_id' => 'SJB0002',
-                'applicant' => 'zhangsan',
-                'department' => 'shengchanbu',
-                'leibie' => 'jiaban1',
-                'kaishi_riqi' => $nowtime,
-                'jiesu_riqi' => $nowtime,
-                'duration' => 60,
-                'liyou' => 'liyou1111',
-                'remark' => 'remark1'
-                ],
-                [
-                'main_id' => 'MJB000000001',
-                'sub_id' => 'SJB0003',
-                'applicant' => 'zhangsan',
-                'department' => 'shengchanbu',
-                'leibie' => 'jiaban2',
-                'kaishi_riqi' => $nowtime,
-                'jiesu_riqi' => $nowtime,
-                'duration' => 60,
-                'liyou' => 'liyou2222',
-                'remark' => ''
-                ],
-            ], JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES | JSON_NUMERIC_CHECK),
+            'info' => '{
+                "main_id": "MJB000000002",
+                "sub_id": "SJB0005",
+                "applicant": "zhangsan",
+                "department": "shengchanbu",
+                "leibie": "shuanxiujiaban",
+                "kaishi_riqi": "' . $nowtime . '",
+                "jiesu_riqi": "' . $nowtime . '",
+                "duration": 60,
+                "liyou": "liyou2",
+                "remark": ""
+            }',
         ]);
         
-        Jiaban::create([
-            'main_id' => 'MJB000000002',
-            'agent' => 'lisi',
-            'department' => 'caiwubu',
-            'info' => json_encode([
-                [
-                'main_id' => 'MJB000000002',
-                'sub_id' => 'SJB0005',
-                'applicant' => 'zhangsan',
-                'department' => 'shengchanbu',
-                'leibie' => 'shuanxiujiaban',
-                'kaishi_riqi' => $nowtime,
-                'jiesu_riqi' => $nowtime,
-                'duration' => 60,
-                'liyou' => 'liyou2',
-                'remark' => ''
-                ]
-            ], JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES | JSON_NUMERIC_CHECK),
-        ]);
+        // Jiaban::create([
+        //     'main_id' => 'MJB000000002',
+        //     'agent' => 'lisi',
+        //     'department' => 'caiwubu',
+        //     'info' => json_encode([
+        //         '{
+        //         "main_id" => "MJB000000002",
+        //         "sub_id" => "SJB0005",
+        //         "applicant" => "zhangsan",
+        //         "department" => "shengchanbu",
+        //         "leibie" => "shuanxiujiaban",
+        //         "kaishi_riqi" => $nowtime,
+        //         "jiesu_riqi" => $nowtime,
+        //         "duration" => 60,
+        //         "liyou" => "liyou2",
+        //         "remark" => ""
+        //         }'
+        //     ], JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES | JSON_NUMERIC_CHECK),
+        // ]);
         
 	
 		
