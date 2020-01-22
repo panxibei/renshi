5e32f36a3a0cc5f747bf630961c5975a17898fc4
panxibei <panxibei@yandex.com>
2020-01-22 08:57:42 +0800

    renshi

--- resources/views/main/portalcube.blade.php
+++ resources/views/main/portalcube.blade.php
@@ -342,17 +342,12 @@ var vm_app = new Vue({
                     trigger: 'item',
                     formatter: '{a} <br/>{b} : {c} ({d}%)'
                 },
-                legend: {
-                    orient: 'vertical',
-                    left: 'left',
-                    data: ['直接访问', '邮件营销', '联盟广告', '视频广告', '搜索引擎']
-                },
                 series: [
                     {
                         name: '访问来源',
                         type: 'pie',
-                        radius: '55%',
-                        center: ['50%', '60%'],
+                        radius: '35%',
+                        center: ['20%', '40%'],
                         data: [
                             {value: 335, name: '直接访问'},
                             {value: 310, name: '邮件营销'},
